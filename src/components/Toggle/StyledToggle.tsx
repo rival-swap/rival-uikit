88c328d33037ac04231cbee3d33b727697da8dbc
Literally Collective <bluedragon.cstar@outlook.com>
2022-04-20 10:59:12 -0300

    Update StyledToggle.tsx

--- src/components/Toggle/StyledToggle.tsx
+++ src/components/Toggle/StyledToggle.tsx
@@ -75,7 +75,7 @@ const StyledToggle = styled.div<StyleToggleProps>`
   align-items: center;
   background-color: ${({ theme, $checked, $checkedColor, $defaultColor }) =>
     theme.colors[$checked ? $checkedColor : $defaultColor]};
-  border-radius: 24px;
+  border-radius: 6px;
   box-shadow: ${({ theme }) => theme.shadows.inset};
   cursor: pointer;
   display: inline-flex;
